3680bd0477daaf7c67c47fc4e2a36361cee33355
lindongjiang <63407529+lindongjiang@users.noreply.github.com>
2023-03-25 22:29:52 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -13,7 +13,12 @@ const app = express();
 
 // 添加中间件
 app.use(express.json());
-app.use(cors());
+// Add CORS headers
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(csrf());
